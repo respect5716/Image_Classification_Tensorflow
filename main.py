cd8addb0f2c73aa1aaa8bb1adadad8368081b9ac
unknown <Administrator@SKCC16N00116.SKCC.NET>
2021-02-08 16:28:04 +0900

    Add wandb and argparse

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import os
+import wandb
 import tensorflow as tf
 import tensorflow_addons as tfa
 
 from utils import Dataloader
 from models import *
 
-def create_loader():
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, default='vgg11')
+parser.add_argument('--batch_size', type=int, default=256)
+parser.add_argument('--epoch_size', type=int, default=200)
+args = parser.parse_args()
+
+def create_loader(batch_size):
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-    train_loader = Dataloader(x_train, y_train, 'train', 256)
-    test_loader = Dataloader(x_test, y_test, 'test', 256)
+    train_loader = Dataloader(x_train, y_train, 'train', batch_size)
+    test_loader = Dataloader(x_test, y_test, 'test', batch_size)
     return train_loader, test_loader
 
 def create_model(model_name):
@@ -28,24 +36,30 @@ def create_model(model_name):
     return model_dict[model_name]()
 
 
-def main(model_name):
-    train_loader, test_loader = create_loader()
+def main(args):
+    wandb.init(
+        project = 'cifar10',
+        config = var(args)
+    )
+
+    train_loader, test_loader = create_loader(args.batch_size)
     model = create_model(model_name)
+    learning_rate = tf.keras.optimizers.schedules.ExponentialDecay(0.01, 1000, 0.95)
     model.compile(
         loss = 'sparse_categorical_crossentropy',
         metrics = ['acc'],
-        optimizer = tf.keras.optimizers.Adam(
-            learning_rate=tf.keras.optimizers.schedules.ExponentialDecay(0.01, 1000, 0.95)
-        )
+        optimizer = tf.keras.optimizers.Adam(learning_rate)
     )
     print(model.summary())
 
     model.fit(
         train_loader,
-        epochs = 10
+        epochs = args.epoch_size,
+        validation_data = test_loader,
+        callbacks = [wandb.keras.WandbCallback()]
     )
 
     model.evaluate(test_loader)
 
 if __name__ == '__main__':
-    main('vgg11')
+    main(args)
